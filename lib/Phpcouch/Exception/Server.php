cef505368c179a728ca181971900ff874925ff5c
David Zuelke <david.zuelke@bitextender.com>
2007-11-16 15:32:52 +0000

    finalize dir structure

--- lib/Phpcouch/Exception/Server.php
+++ lib/Phpcouch/Exception/Server.php
@@ -2,6 +2,24 @@
 
 class PhpcouchServerException extends PhpcouchException
 {
+	protected $status;
+	
+	public function __construct(PhpcouchStatus $status)
+	{
+		parent::__construct($status->error->id, $status->error->reason);
+		
+		$this->status = $status;
+	}
+	
+	public function getCouchdbError()
+	{
+		return $this->status;
+	}
+	
+	public function __toString()
+	{
+		return json_encode($this->status);
+	}
 }
 
 ?>
