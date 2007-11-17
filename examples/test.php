245aa25a1389a3b560367ce6f36f98fa88f48482
David Zuelke <david.zuelke@bitextender.com>
2007-11-17 19:35:20 +0000

    nicer database handling, database methods in the connection, error exception w/ body and error code, json transcoding in the adapter and adjusted test

--- examples/test.php
+++ examples/test.php
@@ -1,12 +1,23 @@
 <?php
 
+error_reporting(E_ALL | E_STRICT);
+
 set_include_path(get_include_path() . ':' . '/Users/dzuelke/Downloads/ZendFramework-1.0.2/library');
 
 require('../lib/Phpcouch.php');
 
 PhpCouch::registerConnection('default', $con = new PhpcouchConnection(array('database' => 'hellohans'), new PhpcouchZendhttpclientAdapter()));
 
+var_dump($con->retrieveDatabase('hellohans'));
+try {
+	var_dump($con->createDatabase('hellohans2'));
+	var_dump($con->deleteDatabase('hellohans2'));
+} catch(PhpcouchClientErrorException $e) {
+}
+
 $doc = $con->retrieve('63A0B00A68EEBE4ECB4E0F8F9682F813');
+$doc->title = 'hello again';
+$doc->save();
 
 $doc = $con->newDocument();
 $doc->_id = uniqid();
