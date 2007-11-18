bd491ece57ada6dcfca6594c8488d1c9761658d1
David Zuelke <david.zuelke@bitextender.com>
2007-11-18 19:54:11 +0000

    docs, docs, docs

--- lib/Phpcouch.class.php
+++ lib/Phpcouch.class.php
@@ -3,10 +3,13 @@
 /**
  * Main PHPCouch class.
  *
- * @author     David Zülke
+ * @package    PHPCouch
+ *
+ * @author     David Zülke <dz@bitxtender.com>
  * @copyright  bitXtender GbR
  *
- * @since      0.1.0
+ * @since      1.0.0
+ *
  * @version    $Id$
  */
 class Phpcouch
@@ -29,6 +32,19 @@ class Phpcouch
 	 */
 	protected static $path = null;
 	
+	/**
+	 * @var        string The name of the default connection.
+	 */
+	protected static $defaultConnection = null;
+	
+	/**
+	 * PHPCouch autoloader.
+	 *
+	 * @param      string The name of the class to autoload.
+	 *
+	 * @author     David Zülke <dz@bitxtender.com>
+	 * @since      1.0.0
+	 */
 	public static function autoload($className)
 	{
 		if(isset(self::$autoloads[$className])) {
@@ -36,8 +52,17 @@ class Phpcouch
 		}
 	}
 	
+	/**
+	 * Main PHPCouch initialization method.
+	 *
+	 * This sets up the base path and registers the autoloader.
+	 *
+	 * @author     David Zülke <dz@bitxtender.com>
+	 * @since      1.0.0
+	 */
 	public static function bootstrap()
 	{
+		// grab the base path where we are located
 		self::$path = dirname(__FILE__);
 		
 		self::$autoloads = array(
@@ -62,13 +87,25 @@ class Phpcouch
 			'PhpcouchIRegistry'             => 'Phpcouch/Registry.interface.php',
 		);
 		
+		// and register our autoloads
 		spl_autoload_register(array('PhpCouch', 'autoload'));
 	}
 	
+	/**
+	 * Version information method.
+	 *
+	 * Returns version number along with the version status, if applicable.
+	 *
+	 * @return     string A version number, including status if applicable, e.g. "1.2.0-RC2".
+	 *
+	 * @author     David Zülke <dz@bitxtender.com>
+	 * @since      1.0.0
+	 */
 	public static function getVersionInfo()
 	{
 		$retval = self::VERSION_NUMBER;
 		
+		// only append a status (like "RC3") if it is set
 		if(self::VERSION_STATUS !== null) {
 			$retval .= '-' . self::VERSION_STATUS;
 		}
@@ -76,30 +113,88 @@ class Phpcouch
 		return $retval;
 	}
 	
+	/**
+	 * Full version information string method.
+	 *
+	 * Returns the product name and the version number along with the version status, if applicable.
+	 *
+	 * @return     string A full version string, example: "PHPCouch/1.0.0".
+	 *
+	 * @author     David Zülke <dz@bitxtender.com>
+	 * @since      1.0.0
+	 */
 	public static function getVersionString()
 	{
+		// a slash is common, e.g. Apache/2.2.23 or PHP/5.2.4, so we do that too
 		return 'PHPCouch/' . self::getVersionInfo();
 	}
 	
-	public static function registerConnection($name, PhpcouchConnection $connection)
+	/**
+	 * Register a connection.
+	 *
+	 * @param      string             The name of the connection.
+	 * @param      PhpcouchConnection A connection instance.
+	 * @param      bool               Whether or not to make this connection the default one.
+	 *
+	 * @author     David Zülke <dz@bitxtender.com>
+	 * @since      1.0.0
+	 */
+	public static function registerConnection($name, PhpcouchConnection $connection, $default = true)
 	{
+		if(!$default && !self::$connections) {
+			$default = true;
+		}
+		
 		self::$connections[$name] = $connection;
+		
+		if($default) {
+			self::$defaultConnection = $name;
+		}
 	}
 	
+	/**
+	 * Unregister a previously registered connection.
+	 *
+	 * @param      string The name of the connection to remove.
+	 *
+	 * @return     PhpcouchConnection The connection instance that was removed from the pool, or null if no connection of that name was registered.
+	 *
+	 * @author     David Zülke <dz@bitxtender.com>
+	 * @since      1.0.0
+	 */
 	public static function unregisterConnection($name)
 	{
 		if(isset(self::$connections[$name])) {
+			// remember the value we are about to remove...
 			$retval = self::$connections[$name];
 			unset(self::$connections[$name]);
+			// ...and return it
 			return $retval;
 		}
 	}
 	
-	public static function getConnection($name = 'default')
+	/**
+	 * Retrieve a registered connection instance.
+	 *
+	 * @param      string The name of the connection, or null (default) if the default connection should be returned.
+	 *
+	 * @return     PhpcouchConnection A connection instance, if found.
+	 *
+	 * @throws     PhpcouchException If no connection of this name was configured.
+	 *
+	 * @author     David Zülke <dz@bitxtender.com>
+	 * @since      1.0.0
+	 */
+	public static function getConnection($name = null)
 	{
-		if(isset(self::$connections[$name])) {
+		if($name === null) {
+			$name = self::$defaultConnection;
+		}
+		
+		if($name && isset(self::$connections[$name])) {
 			return self::$connections[$name];
 		} else {
+			// no default connection set and name was empty, or name is invalid
 			throw new PhpcouchException(sprintf('Connection "%s" not configured.', $name));
 		}
 	}
