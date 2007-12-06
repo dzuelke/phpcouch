676e9eac6de1cd3b1682cb197b65cc4745bb0c8c
David Zuelke <david.zuelke@bitextender.com>
2007-12-06 21:00:42 +0000

    phpdocs and more work on moving all data to overload array

--- lib/Phpcouch/Record.class.php
+++ lib/Phpcouch/Record.class.php
@@ -2,14 +2,39 @@
 
 abstract class PhpcouchRecord implements PhpcouchIRecord
 {
+	/**
+	 * @var        PhpcouchConnection The connection associated with this record.
+	 */
 	protected $connection = null;
+	
+	/**
+	 * @var        array The data array for magic methods.
+	 */
 	protected $data = array();
 	
+	/**
+	 * Record constructor.
+	 *
+	 * @param      PhpcouchConnection An optional connection to associate with this record.
+	 *
+	 * @author     David Zülke <dz@bitxtender.com>
+	 * @since      1.0.0
+	 */
 	public function __construct(PhpcouchConnection $connection = null)
 	{
 		$this->setConnection($connection);
 	}
 	
+	/**
+	 * Get overload.
+	 *
+	 * @param      string Name of the virtual property to fetch.
+	 *
+	 * @return     mixed The property value.
+	 *
+	 * @author     David Zülke <dz@bitxtender.com>
+	 * @since      1.0.0
+	 */
 	public function __get($name)
 	{
 		if(isset($this->data[$name])) {
@@ -17,16 +42,43 @@ abstract class PhpcouchRecord implements PhpcouchIRecord
 		}
 	}
 	
+	/**
+	 * Isset overload.
+	 *
+	 * @param      string Name of the virtual property to check for existance.
+	 *
+	 * @return     bool Whether or not a property of that name exists.
+	 *
+	 * @author     David Zülke <dz@bitxtender.com>
+	 * @since      1.0.0
+	 */
 	public function __isset($name)
 	{
 		return isset($this->data[$name]);
 	}
 	
+	/**
+	 * Set overload.
+	 *
+	 * @param      string Name of the virtual property to set.
+	 * @param      mixed  The value to set.
+	 *
+	 * @author     David Zülke <dz@bitxtender.com>
+	 * @since      1.0.0
+	 */
 	public function __set($name, $value)
 	{
 		$this->data[$name] = $value;
 	}
 	
+	/**
+	 * Unset overload.
+	 *
+	 * @param      string Name of the virtual property to unset.
+	 *
+	 * @author     David Zülke <dz@bitxtender.com>
+	 * @since      1.0.0
+	 */
 	public function __unset($name)
 	{
 		if(array_key_exists($name, $this->data)) {
@@ -34,6 +86,15 @@ abstract class PhpcouchRecord implements PhpcouchIRecord
 		}
 	}
 	
+	/**
+	 * Load data into this record.
+	 * This will clear all information before importing the data.
+	 *
+	 * @param      array The data to load.
+	 *
+	 * @author     David Zülke <dz@bitxtender.com>
+	 * @since      1.0.0
+	 */
 	public function hydrate($data)
 	{
 		$this->data = array();
@@ -41,6 +102,14 @@ abstract class PhpcouchRecord implements PhpcouchIRecord
 		$this->fromArray($data);
 	}
 	
+	/**
+	 * Import data into this record.
+	 *
+	 * @param      mixed The data to load (array, PhpcouchIRecord or object)
+	 *
+	 * @author     David Zülke <dz@bitxtender.com>
+	 * @since      1.0.0
+	 */
 	public function fromArray($data)
 	{
 		if($data instanceof PhpcouchIRecord) {
@@ -58,6 +127,14 @@ abstract class PhpcouchRecord implements PhpcouchIRecord
 		}
 	}
 	
+	/**
+	 * Export data from this record.
+	 *
+	 * @return     array An array representation of this record's data.
+	 *
+	 * @author     David Zülke <dz@bitxtender.com>
+	 * @since      1.0.0
+	 */
 	public function toArray()
 	{
 		$retval = array();
@@ -66,20 +143,32 @@ abstract class PhpcouchRecord implements PhpcouchIRecord
 			$retval[$key] = $this->__get($key);
 		}
 		
-		foreach(get_object_vars($this) as $key => $value) {
-			if(strpos($key, '_') === 0) {
-				$retval[$key] = $value;
-			}
-		}
-		
 		return $retval;
 	}
 	
+	/**
+	 * Retrieve the connection associated with this record.
+	 *
+	 * @return     PhpcouchConnection The connection used by this record.
+	 *
+	 * @author     David Zülke <dz@bitxtender.com>
+	 * @since      1.0.0
+	 */
 	public function getConnection()
 	{
 		return $this->connection;
 	}
 	
+	/**
+	 * Set the connection to be used with this record.
+	 *
+	 * @param      PhpcouchConnection The connection to associate.
+	 *
+	 * @throws     ?
+	 *
+	 * @author     David Zülke <dz@bitxtender.com>
+	 * @since      1.0.0
+	 */
 	public function setConnection(PhpcouchConnection $connection = null)
 	{
 		if($connection === null) {
--- lib/Phpcouch/Record/Mutable.class.php
+++ lib/Phpcouch/Record/Mutable.class.php
@@ -2,19 +2,51 @@
 
 abstract class PhpcouchMutableRecord extends PhpcouchRecord implements PhpcouchIMutableRecord
 {
+	/**
+	 * @var        bool Flag indicating whether or not this record is new.
+	 */
 	protected $isNew = true;
+	
+	/**
+	 * @var        bool Flag indicating whether or not this record is modified.
+	 */
 	protected $isModified = false;
 	
+	/**
+	 * Indicates whether or not this record is new, i.e. never saved to the database before.
+	 *
+	 * @return     bool True, if this record is new.
+	 *
+	 * @author     David Zülke <dz@bitxtender.com>
+	 * @since      1.0.0
+	 */
 	public function isNew()
 	{
 		return $this->isNew;
 	}
 	
+	/**
+	 * Indicates whether or not this record is modified, i.e. changed since the last save.
+	 *
+	 * @return     bool True, if this record is new.
+	 *
+	 * @author     David Zülke <dz@bitxtender.com>
+	 * @since      1.0.0
+	 */
 	public function isModified()
 	{
 		return $this->isModified;
 	}
 	
+	/**
+	 * Set overload.
+	 *
+	 * @param      string Name of the virtual property to set.
+	 * @param      mixed  The value to set.
+	 *
+	 * @author     David Zülke <dz@bitxtender.com>
+	 * @since      1.0.0
+	 */
 	public function __set($name, $value)
 	{
 		if(!isset($this->data[$name]) || $this->data[$name] !== $value) {
@@ -24,6 +56,14 @@ abstract class PhpcouchMutableRecord extends PhpcouchRecord implements PhpcouchI
 		parent::__set($name, $value);
 	}
 	
+	/**
+	 * Unset overload.
+	 *
+	 * @param      string Name of the virtual property to unset.
+	 *
+	 * @author     David Zülke <dz@bitxtender.com>
+	 * @since      1.0.0
+	 */
 	public function __unset($name)
 	{
 		if(array_key_exists($name, $this->data)) {
@@ -33,6 +73,15 @@ abstract class PhpcouchMutableRecord extends PhpcouchRecord implements PhpcouchI
 		parent::__unset($name);
 	}
 	
+	/**
+	 * Load data into this record.
+	 * This will clear all information before importing the data and set new and modified flags to false.
+	 *
+	 * @param      array The data to load.
+	 *
+	 * @author     David Zülke <dz@bitxtender.com>
+	 * @since      1.0.0
+	 */
 	public function hydrate($data)
 	{
 		parent::hydrate($data);
@@ -41,6 +90,14 @@ abstract class PhpcouchMutableRecord extends PhpcouchRecord implements PhpcouchI
 		$this->isModified = false;
 	}
 	
+	/**
+	 * Dehydrate record data into a saveable array.
+	 *
+	 * @return     array The cleaned data array.
+	 *
+	 * @author     David Zülke <dz@bitxtender.com>
+	 * @since      1.0.0
+	 */
 	public function dehydrate()
 	{
 		$data = $this->toArray();
