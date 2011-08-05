2cb2d077b8c759eabd6c41afcc6c9a124b6197b7
Simon Thulbourn <simon+github@thulbourn.com>
2011-08-05 11:25:27 +0100

    adds base class structure to files, refs #1

---
+++ tests/Phpcouch/ConfigurableAbstractTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Configurable Abstract Test
+ *
+ * @package    PHPCouch
+ * @subpackage Tests
+ *
+ * @author     Simon Thulbourn <simon+github@thulbourn.com>
+ * @copyright  authors
+ *
+ * @since      1.0.0
+ */
+class ConfigurableAbstractTest extends PHPUnit_Framework_TestCase
+{
+	
+}
+
+?>
