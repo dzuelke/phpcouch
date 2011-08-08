999facd93dfd88a7e409a88fe6f2ef16fe1592e6
Simon Thulbourn <simon+github@thulbourn.com>
2011-08-08 11:09:44 +0100

    completes test for HttpResponse, refs #2

--- tests/Phpcouch/Http/HttpResponseTest.php
+++ tests/Phpcouch/Http/HttpResponseTest.php
@@ -1,4 +1,10 @@
 <?php
+
+use phpcouch\Phpcouch;
+use phpcouch\Exception;
+use phpcouch\connection;
+use phpcouch\adapter;
+
 /**
  * Phpcouch Test
  *
@@ -12,16 +18,47 @@
  */
 class HttpResponseTest extends PHPUnit_Framework_TestCase
 {
-	/**
-	 * Placeholder test, keep until actual tests are written
-	 * 
-	 * @assert     true
-	 * 
-	 * @author     Simon Thulbourn <simon+github@thulbourn.com>
-	 */
-	public function testPlaceholder()
+	public function setUp()
 	{
-		$this->assertTrue(true);
+		Phpcouch::bootstrap();
+	}
+	
+	/**
+	 * Test Preceeding Response
+	 *
+	 * @assert       same
+	 *
+	 * @author       Simon Thulbourn <simon+github@thulbourn.com>
+	 */
+	public function testPrecedingResponse()
+	{
+		$precedingResponse = new \phpcouch\http\HttpResponse();
+		$precedingResponse->setStatusCode(100);
+		
+		$response = new \phpcouch\http\HttpResponse();
+		$response->setPrecedingResponse($precedingResponse);
+		
+		$this->assertSame($response->getPrecedingResponse(), $precedingResponse);
+	}
+	
+	/**
+	 * Test Status code
+	 *
+	 * @assert       equals
+	 * @assert       true
+	 * @assert       false
+	 *
+	 * @author       Simon Thulbourn <simon+github@thulbourn.com>
+	 */
+	public function testStatusCode()
+	{
+		$response = new \phpcouch\http\HttpResponse();
+		$response->setStatusCode(100);
+		
+		$this->assertEquals($response->getStatusCode(100), 100);
+		
+		$this->assertTrue($response->validateHttpStatusCode(100));
+		$this->assertFalse($response->validateHttpStatusCode(1));
 	}
 }
 
