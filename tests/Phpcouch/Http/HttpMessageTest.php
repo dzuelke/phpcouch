84471e95e44e9431ced171af56a85f0185635f75
Simon Thulbourn <simon+github@thulbourn.com>
2011-08-08 11:09:02 +0100

    completes tests for HttpMessage, refs #2

--- tests/Phpcouch/Http/HttpMessageTest.php
+++ tests/Phpcouch/Http/HttpMessageTest.php
@@ -1,4 +1,10 @@
 <?php
+
+use phpcouch\Phpcouch;
+use phpcouch\Exception;
+use phpcouch\connection;
+use phpcouch\adapter;
+
 /**
  * Phpcouch Test
  *
@@ -13,15 +19,99 @@
 class HttpMessageTest extends PHPUnit_Framework_TestCase
 {
 	/**
-	 * Placeholder test, keep until actual tests are written
+	 * Setup
+	 */
+	public function setUp()
+	{
+		Phpcouch::bootstrap();
+	}
+	
+	/**
+	 * Test header functionality
 	 * 
 	 * @assert     true
+	 * @assert     equals
+	 * @assert     empty
 	 * 
 	 * @author     Simon Thulbourn <simon+github@thulbourn.com>
 	 */
-	public function testPlaceholder()
+	public function testHeader()
 	{
-		$this->assertTrue(true);
+		$message = new \phpcouch\http\HttpMessage();
+		
+		$data = array(
+			// input => expected
+			'content-type' => 'Content-Type',
+			'etag' => 'ETag',
+			'wWw-authEnticate' => 'WWW-Authenticate'
+		);
+		
+		foreach ($data as $k => $v) {
+			$this->assertEquals($message->normalizeHttpHeaderName($k), $v);
+		}
+		
+		$message->setHeader('Content-Type', 'text/plain; charset=utf-8');
+		$this->assertTrue($message->hasHeader('Content-Type'));
+		$this->assertEquals($message->getHeader('Content-Type'), array('text/plain; charset=utf-8'));
+		
+		$message->setHeader('Content-Type', 'application/json; charset=utf-8', false);
+		$this->assertEquals($message->getHeader('Content-Type'), array('text/plain; charset=utf-8'));
+		
+		$this->assertEquals($message->removeHeader('Content-Type'), array('text/plain; charset=utf-8'));
+		$this->assertEmpty($message->getHeaders(), array());
+		
+		$data = array(
+			'Content-Type' => 'text/plain; charset=utf-8',
+			'Content-Length' => 0
+		);
+		
+		foreach ($data as $k => &$v) {
+			$message->setHeader($k, $v, true);
+			
+			$v = array($v);
+		}
+		
+		$this->assertEquals($message->getHeaders(), $data);
+		
+		$message->clearHeaders();
+		$this->assertEmpty($message->getHeaders());
+	}
+	
+	/**
+	 * Test content type
+	 * 
+	 * @assert     equals
+	 * 
+	 * @author     Simon Thulbourn <simon+github@thulbourn.com>
+	 */
+	public function testContentType()
+	{
+		$message = new \phpcouch\http\HttpMessage();
+		
+		$message->setContentType('text/plain; charset=utf-8');
+		$this->assertEquals($message->getContentType(), 'text/plain; charset=utf-8');
+	}
+	
+	/**
+	 * Test content
+	 * 
+	 * @assert     equals
+	 * 
+	 * @author     Simon Thulbourn <simon+github@thulbourn.com>
+	 */
+	public function testContent()
+	{
+		$content = 'The quick brown fox jumps over the lazy dog';
+		
+		$message = new \phpcouch\http\HttpMessage();
+		
+		$message->setContent($content);
+		$this->assertEquals($message->getContent(), $content);
+		
+		$this->assertEquals($message->getContentSize(), strlen($content));
+		
+		$message->clearContent();
+		$this->assertEmpty($message->getContent());
 	}
 }
 
